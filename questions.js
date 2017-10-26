70bba2438c822c34f9325051945be720f4d120cd
AndreVictor60 <andre.victor60@free.fr>
2017-10-26 12:29:26 +0200

    Erreur

--- questions.js
+++ questions.js
@@ -4,97 +4,61 @@
  * Il peut y avoir plusieur façon de faire.
  */
 var tailleString = function (texte) {
-  // Taille de la chaîne
-    var result = texte.length;
-    return result;
+    return 'A completer';
 }
 var remplaceECar = function (texte) {
-  // Remplace le e par espace
-    var result = texte.replace("e", " ");
-    return result;
+    return 'A completer';
 }
 var concatString = function (texte1, texte2) {
-  // Concaténation de deux chaînes
-    var result = texte1 + texte2;
-    return result;
+    return 'A completer';
 }
 var afficherCar5 = function (texte) {
-  // Récuperer le 5 caractére
-    var result = texte.charAt(4);
-    return result;
+    return 'A completer';
 }
 var afficher9Car = function (texte) {
-  //Les chaines de caractères : Afficher les 9 premiers caractères
-    var result = texte.slice(0,9);
-    return result;
+    return 'A completer';
 }
 var majusculeString = function (texte) {
-  // Les chaines de caractères : Mettre en majuscule la chaine
-    var result = texte.toLocaleUpperCase();
-    return result;
+    return 'A completer';
 }
 var minusculeString = function (texte) {
-  // Les chaines de caractères : Mettre en minuscule la chaine
-    var result = texte.toLocaleLowerCase();
-    return result;
+    return 'A completer';
 }
 var SupprEspaceString = function (texte) {
-  // Les chaines de caractères : Supprimer les espaces avant et après la chaine
-    var result = texte.trim()
-      return result;
+
+      return 'A completer';
 }
 var IsString = function (texte) {
-  //Les chaines de caractères : Afficher true si le parametre d'entrée de la fonction est de type string
-  var result = typeof(texte) === 'string';
-    return result;
+    return 'A completer';
 }
 var AfficherExtensionString = function (texte) {
-  // Les chaines de caractères : Afficher l'extension du fichier
-    var result = texte.substring(texte.lastIndexOf(".")+1, texte.length);
-    return result;
+    return 'A completer';
 }
 var NombreEspaceString = function (texte) {
-  // Les chaines de caractères : Compter le nombre d'espace dans la chaine
-    var result = texte.split(' ').length - 1;
-    return result;
+    return 'A completer';
 }
 var InverseString = function (texte) {
-  // Les chaines de caractères : Inverser une chaine de caractères
-  var result = texte.split('').reverse().join('');
-    return result;
+    return 'A completer';
 }
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
 var calculPuissance = function (x, y) {
-    var result = Math.pow(x,y);
-    return result;
+    return 'A completer';
 }
 var valeurAbsolue = function (nombre) {
-    var result = Math.abs(nombre);
-    return result;
+    return 'A completer';
 }
 var valeurAbsolueArray = function (array) {
-  //Les nombres et opérations mathématiques : Afficher les valeurs absolues de plusieurs nombres
-    var result = array.map(Math.abs);
-    return result;
+    return 'A completer';
 }
 var sufaceCercle = function (rayon) {
-  //Les nombres et opérations mathématiques : Calculer la surface d'un cercle en fonction de son rayon. L'arondir à l'entier le plus proche
-    var result = Math.PI * (rayon * rayon);
-    var result = Math.round(result);
-    return result;
+    return 'A completer';
 }
 var hypothenuse = function (ab, ac) {
-  // Les nombres et opérations mathématiques : Calculer l'hypthènuse d'un triangle rectangle
-    var result =  Math.hypot(ab, ac);
-    return result;
+    return 'A completer';
 }
 var calculIMC = function (poids, taille) {
-  //Les nombres et opérations mathématiques : Calculer l'IMC (Poids / (taille x taille).Ne garder que deux chiffres après la virgule.
-    var result = poids /(taille * taille);
-    result = result.toFixed(2);
-    result = parseFloat(result);
-    return result;
+    return 'A completer';
 }
